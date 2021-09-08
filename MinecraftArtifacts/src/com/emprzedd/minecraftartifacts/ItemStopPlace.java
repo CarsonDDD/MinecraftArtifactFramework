139874ac4c2c18ddf9f4a6753488ece0462ba1eb
Computer <carson-dickinson@hotmail.com>
2021-09-07 20:12:07 -0500

    2021/09/07

--- MinecraftArtifacts/src/com/emprzedd/minecraftartifacts/ItemStopPlace.java
+++ MinecraftArtifacts/src/com/emprzedd/minecraftartifacts/ItemStopPlace.java
@@ -1,6 +1,7 @@
 package com.emprzedd.minecraftartifacts;
 
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
 
@@ -8,12 +9,14 @@ import com.emprzedd.minecraftartifacts.items.ArtifactItem;
 
 public class ItemStopPlace implements Listener {
 
-	@EventHandler
-	public void onSignPlace(BlockPlaceEvent e) {
+	@EventHandler(priority = EventPriority.HIGHEST)
+	public void onBlockPlace(BlockPlaceEvent e) {
 		/*ArtifactItem mainHand = ArtifactItem.converItemToArtifact(e.getPlayer().getInventory().getItemInMainHand());
 		ArtifactItem offHand = ArtifactItem.converItemToArtifact(e.getPlayer().getInventory().getItemInOffHand());*/
 		ArtifactItem item = ArtifactItem.convertItemToArtifact(e.getItemInHand());
-		if(item !=null &&!item.canPlace/*(mainHand != null && !mainHand.canPlace) || (offHand != null && !offHand.canPlace)*/)
+		if(item !=null &&!item.canPlace/*(mainHand != null && !mainHand.canPlace) || (offHand != null && !offHand.canPlace)*/) {
+			e.setCancelled(true);
 			e.setBuild(false);	
+		}
 	}
 }
--- MinecraftArtifacts/src/com/emprzedd/minecraftartifacts/items/LockettePick.java
+++ MinecraftArtifacts/src/com/emprzedd/minecraftartifacts/items/LockettePick.java
@@ -1,15 +1,20 @@
 package com.emprzedd.minecraftartifacts.items;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.bukkit.Material;
 import org.bukkit.Sound;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.attribute.AttributeModifier.Operation;
 import org.bukkit.block.Sign;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -20,9 +25,13 @@ public class LockettePick extends ArtifactItem{
 
 	float successChance = 0.25f;
 	int DURABILITY_COST =16;
+	int startingDurability = 10;
+	
 	String TARGET_SIGN = "[private]";
 	String MESSAGE_SUCCESS = "lock was brocken";
 	String MESSAGE_FAIL = "lock pick attemped failed";
+	
+
 	
 	public LockettePick(String rawName, Material type, String lore) {
 		super(rawName,rawName, type, lore);
@@ -34,59 +43,13 @@ public class LockettePick extends ArtifactItem{
 
 	@Override
 	protected void init() {
-		
 		this.addUnsafeEnchantment(Enchantment.VANISHING_CURSE, 1);
-		
-		
+	
 		super.canDropItem = true;
 		super.canPlaceInItemFrame=true;
 		super.canPlaceInInventory = true;
 	}
 	
-	@EventHandler(priority = EventPriority.HIGH)
-	public void onUnlockAttempt2(PlayerInteractEvent e) {
-		if(isSelectedArtifact(e.getPlayer().getInventory().getItemInMainHand()) && e.getClickedBlock().getState() instanceof Sign && ((Sign)e.getClickedBlock().getState()).getLine(0).toLowerCase().contains(TARGET_SIGN.toLowerCase())) {
-			e.setCancelled(true);//stops other events from interacting
-			
-			Player player = e.getPlayer();
-			ItemStack item = e.getPlayer().getInventory().getItemInMainHand();
-			
-			String[] bottomLore = item.getItemMeta().getLore().get(item.getItemMeta().getLore().size()-1).split(": ");
-			
-			int usesRemaining = 1;
-			try {
-				usesRemaining= Integer.parseInt(bottomLore[bottomLore.length-1]);
-			}
-			finally {
-				
-				//handle effect
-				if(Math.random() < successChance) {
-					player.sendMessage(ChatColor.translateAlternateColorCodes('&', FORMAT_ALLOW+MESSAGE_SUCCESS));
-					player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_PLACE, 0.5f, 1.5f+(float)(Math.random()/2f));
-					
-					e.getClickedBlock().setType(Material.AIR);
-				}
-				else {
-					player.sendMessage(ChatColor.translateAlternateColorCodes('&', FORMAT_WARN+MESSAGE_FAIL));
-					player.playSound(player.getLocation(), Sound.ENTITY_ZOMBIE_ATTACK_IRON_DOOR, 0.25f, 1f+(float)Math.random());
-				}
-				
-				
-				if(Math.random() < 0.75f)
-					usesRemaining-=1;
-				
-				if(usesRemaining <=0)
-					player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);		//this is extra, lock picks should never stack, this code is redundant
-				else {
-					List<String> lore = item.getItemMeta().getLore();
-					lore.set(lore.size()-1, bottomLore[0]+": "+usesRemaining);
-					
-					item.getItemMeta().setLore(lore);
-				}
-			}
-	
-		}
-	}
 	
 	//@EventHandler(priority = EventPriority.HIGH)
 	public void onUnlockAttempt(PlayerInteractEvent e) {
