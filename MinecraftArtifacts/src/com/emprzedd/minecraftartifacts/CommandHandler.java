7ffcc127906464b1b0ee1288bdc9f5e5280950fc
Carson <carson-dickinson@hotmail.com>
2021-11-13 21:30:16 -0600

    Structure Change

--- MinecraftArtifacts/src/com/emprzedd/minecraftartifacts/CommandHandler.java
+++ MinecraftArtifacts/src/com/emprzedd/minecraftartifacts/CommandHandler.java
@@ -13,7 +13,7 @@ import org.bukkit.entity.Player;
 
 import com.emprzedd.minecraftartifacts.items.ArtifactItem;
 
-public class CommandHandler implements CommandExecutor,TabCompleter{
+public class CommandHandler implements CommandExecutor/*, TabCompleter*/{
 	
 	ArtifactSelectionGUI gui;
 	
@@ -23,48 +23,56 @@ public class CommandHandler implements CommandExecutor,TabCompleter{
 	
 	@Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-        if(!(sender instanceof Player)) {
-        	Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "You must be a player to use this command.");
-        	return true;
-        }
-        
-       
-        Player player = (Player)sender;
-        
-        if(!player.hasPermission("MinecraftArtifacts.Usage")){
-            player.sendMessage(ChatColor.RED + "You do not have permission to use this command.");
-            return true;
-        }
-
-        if(cmd.getName().equalsIgnoreCase("artifact")){
-        	
-        	if(args.length ==0) {
-        		player.openInventory(gui.getInventory());
-        		return true;
-        	}
-        	else if(args.length>=2) {
-        		if(args[0].equalsIgnoreCase("give")) {
-        			player.sendMessage(ChatColor.RED+"This is not implemented yet");
-        			return true;
-        		}
-        		
-        		player.sendMessage(ChatColor.RED+"uh oh, you used this command wrong");
-        	}
-
-        }
-
+    	
+    	if(args.length >= 1 && args[0].equalsIgnoreCase("give")) {
+    		sender.sendMessage(ChatColor.RED+"This is not implemented yet");
+    	}
+    	else if(args.length >= 1 && args[0].equalsIgnoreCase("gui")) {
+    		execGui(sender,cmd,label,args);
+    	}
+    	else if(args.length >= 1 && args[0].equalsIgnoreCase("reload")) {
+    		execReload(sender,cmd,label,args);
+    	}
+    	// DEFAULT COMMAND TREE/HELP command
+    	else {
+    		sender.sendMessage("Test, This is from MinecraftArtifacts");
+    	}
 
         return true;
-    }
+    }	
+	
+	private void execReload(CommandSender sender, Command cmd, String label, String[] args) {
+		if(sender.hasPermission("MinecraftArtifacts.Admin")) {
+			ArtifactItem.reloadAllArtifacts();	
+			sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&4&lGOD&r&f: Artifacts reloaded!"));
+		}
+		else {
+			// plebs
+		}
+	}
+	
+	private void execGui(CommandSender sender, Command cmd, String label, String[] args) {
+        if(sender instanceof Player && sender.hasPermission("MinecraftArtifacts.Admin")) {
+        	((Player)sender).openInventory(gui.getInventory());
+        }
+        else if(sender.hasPermission("MinecraftArtifacts.Admin")) {
+        	Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&4&lGOD&r&f: You must be a player to use this command."));
+        }
+        else {
+        	// plebs
+        }
+	}
 
-	@Override
+	/*@Override
 	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
         if (command.getName().equalsIgnoreCase("artifact")) { // checking if my command is the one i'm after
 
             List<String> autoCompletes = new ArrayList<>(); //create a new string list for tab completion
-
-            if (args.length == 1) { //only interested in the first sub command, if you wanted to cover more deeper sub commands, you could have multiple if statements or a switch statement
+            if(args.length == 0) {
+            	autoCompletes.add("reload");
             	autoCompletes.add("give");
+            	autoCompletes.add("give");
+            	autoCompletes.add("help");
             }
             else if(args.length ==2 && args[0].equalsIgnoreCase("give")) {
             	for(ArtifactItem item : ArtifactItem.ArtifactList)
@@ -75,7 +83,7 @@ public class CommandHandler implements CommandExecutor,TabCompleter{
             return autoCompletes; // then return the list
         }
 
-        return null; // this will return nothing if it wasn't the disguise command I have
-	}
+        return null;
+	}*/
 
 }
