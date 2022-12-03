0990e10fb2ef6e1a70c98f4213b79a8655018c41
Carson <carson-dickinson@hotmail.com>
2022-12-02 19:39:02 -0600

    Feature finished FlameOfCreation

--- src/main/java/me/emprzedd/artifactframework/items/FlameOfCreation.java
+++ src/main/java/me/emprzedd/artifactframework/items/FlameOfCreation.java
@@ -18,7 +18,9 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.HorseJumpEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.event.vehicle.VehicleMoveEvent;
 import org.bukkit.inventory.AbstractHorseInventory;
@@ -34,6 +36,7 @@ import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 import org.bukkit.util.BoundingBox;
@@ -56,6 +59,8 @@ public class FlameOfCreation extends ArtifactItem implements Listener {
 
     private double speedMultiplier = 4;
 
+    private long liveSeconds = 60;
+
     private long summonTime = -cooldownSeconds;
 
     private UUID vehicleId;
@@ -76,83 +81,119 @@ public class FlameOfCreation extends ArtifactItem implements Listener {
 
         Scoreboard board = Bukkit.getScoreboardManager().getMainScoreboard();
         team = board.getTeam("redGlow") == null ? board.registerNewTeam("redGlow") : board.getTeam("redGlow");
-        team.setColor(ChatColor.RED);
+        team.setColor(ChatColor.WHITE);
     }
 
     @Override
     protected void reloadConfig() {
         cooldownSeconds = getConfig().getInt("FlameOfCreation.cooldownSeconds");
         speedMultiplier = getConfig().getDouble("FlameOfCreation.speedMultiplier");
+        liveSeconds = getConfig().getLong("FlameOfCreation.durationSeconds");
     }
 
+    // Jank
     @EventHandler
     public void summon(PlayerInteractEvent e){
         ItemStack item = e.getItem();
         if(item!= null && !item.getType().isAir() && this.isSelectedArtifact(item)){
 
-            if(e.getClickedBlock() == null || !e.getClickedBlock().getRelative(BlockFace.UP).getType().isAir()){
-                screamAtPlayer(e.getPlayer(),"Invalid Spot.");
-                return;
+            // Infinite flint and steel.
+            if(e.getAction() == Action.RIGHT_CLICK_BLOCK){
+                if(e.getClickedBlock() == null || !e.getClickedBlock().getRelative(BlockFace.UP).getType().isAir()){
+                    screamAtPlayer(e.getPlayer(),"Invalid Spot.");
+                    return;
+                }
+                e.getClickedBlock().getRelative(BlockFace.UP).setType(Material.FIRE); // Might destroy top block. Also works around towny
             }
+            // Donkey Summon
+            else if(e.getAction() == Action.LEFT_CLICK_BLOCK){
 
-            e.getClickedBlock().getRelative(BlockFace.UP).setType(Material.FIRE); // Might destroy top block. Also works around towny
+                // Verify proper spot
+                Block summonPoint = e.getClickedBlock().getWorld().getHighestBlockAt(e.getClickedBlock().getLocation());
 
-            // Summon Donkey
-            if(e.getAction() == Action.LEFT_CLICK_BLOCK){
+                if(e.getClickedBlock() == null || !summonPoint.getRelative(BlockFace.UP).getType().isAir()){
+                    screamAtPlayer(e.getPlayer(),"Invalid Spot.");
+                    return;
+                }
 
                 long timeRemaining = summonTime + (cooldownSeconds*1000L) - System.currentTimeMillis();
                 if(timeRemaining > 0){
-                    screamAtPlayer(e.getPlayer(),"A Creation will be ready in : " + timeRemaining/1000 + "s.");
+                    screamAtPlayer(e.getPlayer(),"Creation will be ready in : " + timeRemaining/1000 + "s.");
                     return;
                 }
 
 
-                Donkey donkey = (Donkey) e.getClickedBlock().getLocation().getWorld().spawnEntity(
-                        e.getClickedBlock().getRelative(BlockFace.UP).getLocation(),
-                        EntityType.DONKEY
+                // Spot is Valid, begin summon.
+                summonPoint.getRelative(BlockFace.UP).setType(Material.FIRE); // Might destroy top block. Also works around towny
+                summonPoint.getLocation().getWorld().strikeLightning(summonPoint.getLocation());
+
+                Horse avatar = (Horse) e.getClickedBlock().getLocation().getWorld().spawnEntity(
+                        summonPoint.getRelative(BlockFace.UP,16).getLocation(),
+                        EntityType.HORSE
                 );
-                vehicleId = donkey.getUniqueId();
 
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        avatar.setHealth(0);
+                    }
+                }.runTaskLater(this.getPlugin(), 20L * liveSeconds /*<-- the delay */);
 
-                donkey.setRemoveWhenFarAway(false);
-                donkey.setPersistent(true);
+                e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING,20*20,2,false,false));
+                avatar.setVelocity(new Vector(avatar.getLocation().getDirection().multiply(speedMultiplier).getX(), speedMultiplier, avatar.getLocation().getDirection().multiply(speedMultiplier).getZ()));
+                avatar.getWorld().setThundering(false);
+                vehicleId = avatar.getUniqueId();
 
-                donkey.setFireTicks(Integer.MAX_VALUE);
-                donkey.setVisualFire(true);
+                //avatar.setGliding(true);
+                avatar.setStyle(Horse.Style.NONE);
+                avatar.setColor(Horse.Color.WHITE);
+                avatar.setGlowing(true);
+                team.addEntry(avatar.getUniqueId().toString());
 
-                donkey.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE,Integer.MAX_VALUE,10,false,false));
-                donkey.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING,Integer.MAX_VALUE,2,false,false));
-                donkey.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION,20*3,3,false,false));
-                donkey.addPotionEffect(new PotionEffect(PotionEffectType.SPEED,Integer.MAX_VALUE,3,false,false));
-                donkey.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION,Integer.MAX_VALUE,1,false,false));
+                avatar.setRemoveWhenFarAway(false);
+                avatar.setPersistent(true);
 
-                donkey.setAdult();
-                donkey.setMaxHealth(50);
-                donkey.setJumpStrength(2);
-                donkey.getInventory().setSaddle(new ItemStack(Material.SADDLE));
-                donkey.setEatingHaystack(true);
-                donkey.setLoveModeTicks(Integer.MAX_VALUE);
-                donkey.setOwner(e.getPlayer());
+                //avatar.setFireTicks(Integer.MAX_VALUE);
+                avatar.setVisualFire(true);
 
-                donkey.setCollidable(false);
-                donkey.setAI(true);
-                team.addEntry(donkey.getUniqueId().toString());
-                donkey.setGlowing(true);
-                donkey.setTarget(e.getPlayer());
+                avatar.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE,Integer.MAX_VALUE,10,false,false));
+                avatar.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING,Integer.MAX_VALUE,2,false,false));
+                //avatar.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION,30,3,false,false));
+                avatar.addPotionEffect(new PotionEffect(PotionEffectType.SPEED,Integer.MAX_VALUE,3,false,false));
+                avatar.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION,Integer.MAX_VALUE,1,false,false));
+                avatar.addPotionEffect(new PotionEffect(PotionEffectType.GLOWING,Integer.MAX_VALUE,1,false,false));
 
-                donkey.setCustomNameVisible(true);
-                donkey.setCustomName("Flame Of Creation");
+                avatar.setAdult();
+                avatar.setMaxHealth(50);
+                avatar.setFallDistance(-64);
+                avatar.setJumpStrength(2);
+                avatar.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.27);
+                avatar.getInventory().setSaddle(new ItemStack(Material.SADDLE));
+                avatar.setEatingHaystack(true);
+                avatar.setLoveModeTicks(Integer.MAX_VALUE);
+                avatar.setOwner(e.getPlayer());
 
-                donkey.getLocation().getWorld().strikeLightning(donkey.getLocation());
+                //avatar.setCollidable(false);
+                avatar.setAI(true);
 
+                avatar.setTarget(e.getPlayer());
+
+                avatar.setCustomNameVisible(true);
+                avatar.setCustomName(ChatColor.translateAlternateColorCodes('&',"&6&lAvatar of Solaris Erses"));
+
+                avatar.setPassenger(e.getPlayer());
 
                 e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE,20*20,10,false,false));
-                for (Entity ent : donkey.getNearbyEntities(8,4,8)) {
+                for (Entity ent : avatar.getNearbyEntities(12,64,12)) {
                     if(ent instanceof Player){
-                        ((Player)ent).addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE,20*3,10,false,false));
-                        ((Player)ent).addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION,20*5,1,false,false));
-                        ((Player)ent).addPotionEffect(new PotionEffect(PotionEffectType.SATURATION,20*5,2,false,false));
-                        ent.setFireTicks(20*20);
+                        Player player = (Player)ent;
+                        player.sendMessage(ChatColor.translateAlternateColorCodes('&',"&oThe warmth of the flame inspires you."));
+                        player.addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE,20*3,10,false,false));
+                        player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION,20*5,1,false,false));
+                        player.addPotionEffect(new PotionEffect(PotionEffectType.SATURATION,20*5,2,false,false));
+                        player.setFireTicks(20*20);
+                        player.playSound(avatar.getLocation(),Sound.ENTITY_HORSE_ANGRY,5,0.75f);
+
                     }
                     else if(ent instanceof Monster){
                         ent.setFireTicks(20*10);
@@ -172,13 +213,55 @@ public class FlameOfCreation extends ArtifactItem implements Listener {
         if(!super.hasInInventory(holder.getInventory()))
             return;
 
-        e.getDamager().setFireTicks(20*4);
+        e.getDamager().setFireTicks(20*3);
     }
 
+
     @EventHandler
-    public void onMove(VehicleMoveEvent e){
-        if(e.getVehicle().getUniqueId() == vehicleId){
-            e.getVehicle().setVelocity(new Vector(e.getVehicle().getLocation().getDirection().multiply(speedMultiplier).getX(), 0, e.getVehicle().getLocation().getDirection().multiply(speedMultiplier).getZ()));
+    public void onMove(PlayerMoveEvent e){
+
+        if(e.getPlayer().isInsideVehicle() && e.getPlayer().getVehicle() instanceof Horse){
+
+            Horse avatar = (Horse)e.getPlayer().getVehicle();
+            if(avatar.getUniqueId() == vehicleId){
+                for (Entity ent : avatar.getNearbyEntities(16,2,16)) {
+                    if(ent instanceof Vehicle && ent instanceof LivingEntity){
+                        // Apply slowfall effect
+                        ((LivingEntity)ent).addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING,20*10,20,false,false));
+                    }
+                    else if(!ent.equals(e.getPlayer()) && ent.getLocation().distance(avatar.getLocation()) < 2 && (ent instanceof Monster || ent instanceof Player)){
+                        // Ram Effect
+                        e.getPlayer().playSound(avatar.getLocation(),Sound.ENTITY_ENDER_DRAGON_SHOOT,0.75f,1f);
+                        ((LivingEntity)ent).addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION,5,10,false,false));
+                        ent.setFireTicks(30);
+                        if(ent instanceof Player){
+                            ((Player)ent).playSound(avatar.getLocation(),Sound.ENTITY_ENDER_DRAGON_SHOOT,0.75f,1f);
+                        }
+                    }
+                }
+
+                // Rider Effects
+                e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION,40,1,false,false));
+                e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.GLOWING,40,1,false,false));
+                e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.FIRE_RESISTANCE,40,2,false,false));
+                e.getPlayer().addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION,40,2,false,false));
+
+            }
+        }
+    }
+
+
+    @EventHandler
+    public void onHorseJump(HorseJumpEvent e){
+        if(e.getEntity().getUniqueId() == vehicleId){
+            Horse avatar = (Horse) e.getEntity();
+            avatar.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING,20*10,10,false,false));
+            avatar.getPassengers().forEach( livingEntity -> {
+                if(livingEntity instanceof LivingEntity){
+                    ((LivingEntity) livingEntity).addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING,20*10,2,false,false));
+                }
+            });
+
         }
     }
 }
